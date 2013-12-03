34afc26baf0744182428ec8f0d01c1cca7ee5036
Martin Ueding <dev@martin-ueding.de>
2013-12-03 14:28:52 +0100

    Force tiling

--- rc.lua
+++ rc.lua
@@ -339,9 +339,13 @@ awful.rules.rules = {
       properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     { rule = { class = "Firefox" },
-      properties = { tag = tags[1][1] } },
+      properties = { tag = tags[1][1], floating = false } },
     { rule = { class = "Thunderbird" },
-      properties = { tag = tags[1][2] } },
+      properties = { tag = tags[1][2], floating = false } },
+    { rule = { class = "Konsole" },
+      properties = { floating = false } },
+    { rule = { class = "Dolphin" },
+      properties = { floating = false } },
 }
 -- }}}
 
