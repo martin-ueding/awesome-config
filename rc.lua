d0a3b30ecf4b59f0d88ac05692485569dcf32d8d
Martin Ueding <dev@martin-ueding.de>
2013-12-03 14:01:08 +0100

    Use other tiling layout

--- rc.lua
+++ rc.lua
@@ -55,8 +55,8 @@ modkey = "Mod4"
 layouts =
 {
     awful.layout.suit.floating,
-    awful.layout.suit.tile,
-    -- awful.layout.suit.tile.left,
+    -- awful.layout.suit.tile,
+    awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
     -- awful.layout.suit.tile.top,
     awful.layout.suit.fair,
