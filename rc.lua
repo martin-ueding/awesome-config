604b7a299bbc7ab9bacb9407c385386bc24f87f8
Martin Ueding <dev@martin-ueding.de>
2013-12-10 21:07:18 +0100

    Change bat widget, omit shift for kill

--- rc.lua
+++ rc.lua
@@ -106,7 +106,7 @@ spacer.text = " | "
 
 -- battery widget
 batwidget = widget({ type = "textbox" })
-vicious.register(batwidget, vicious.widgets.bat, "Akku: $1 $2% <span color='white'>$3 h</span>", 15, "BAT0")
+vicious.register(batwidget, vicious.widgets.bat, "Akku: $1 $2% <span color='white'>$3</span>", 15, "BAT0")
 
 -- Create a textclock widget
 -- Register widget
@@ -273,7 +273,7 @@ globalkeys = awful.util.table.join(
 
 clientkeys = awful.util.table.join(
     awful.key({ modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
-    awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end),
+    awful.key({ modkey,           }, "c",      function (c) c:kill()                         end),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
     awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
