3510efa3bd5714073fb31c9911f33d8dd5496eea
Martin Ueding <dev@martin-ueding.de>
2013-12-10 15:44:12 +0100

    Use my own background image

--- rc.lua
+++ rc.lua
@@ -39,7 +39,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init("/home/mu/.config/awesome/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "x-terminal-emulator"
