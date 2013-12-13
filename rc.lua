c05c4cfeebf7aaa86c21969bc2e86ebc635da1bb
Martin Ueding <dev@martin-ueding.de>
2013-12-13 13:38:00 +0100

    Scale Plasma-desktop to a fixed size

--- rc.lua
+++ rc.lua
@@ -343,32 +343,49 @@ root.keys(globalkeys)
 -- {{{ Rules
 awful.rules.rules = {
     -- All clients will match this rule.
-    { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = true,
-                     keys = clientkeys,
-                     maximized_vertical = false,
-                     maximized_horizontal = false,
-                     buttons = clientbuttons,
-                 } },
-    { rule = { class = "pinentry" },
-      properties = { floating = true } },
-    { rule = { class = "gimp" },
-      properties = { floating = true } },
-    { rule = { class = "Plasma-desktop" },
-      properties = { floating = true } },
-    { rule = { class = "Plasma" },
-      properties = { floating = true } },
-    -- Set Firefox to always map on tags number 2 of screen 1.
-    { rule = { class = "Firefox" },
-      properties = { tag = tags[1][1] } },
-    { rule = { class = "Rekonq" },
-      properties = { tag = tags[1][1] } },
-    { rule = { class = "Skype" },
-      properties = { tag = tags[1][8] } },
-    { rule = { class = "Thunderbird" },
-      properties = { tag = tags[1][2] } },
+    {
+        rule = { },
+        properties = {
+            border_width = beautiful.border_width,
+            border_color = beautiful.border_normal,
+            focus = true,
+            keys = clientkeys,
+            maximized_vertical = false,
+            maximized_horizontal = false,
+            buttons = clientbuttons,
+        }
+    },
+    {
+        rule = { class = "pinentry" },
+        properties = { floating = true }
+    },
+    {
+        rule = { class = "gimp" },
+        properties = { floating = true }
+    },
+    {
+        rule = { class = "Plasma-desktop" },
+        properties = { floating = true },
+        callback = function(c)
+            c:geometry( { width = 600 , height = 500 } )
+        end,
+    },
+    {
+        rule = { class = "Firefox" },
+        properties = { tag = tags[1][1] }
+    },
+    {
+        rule = { class = "Rekonq" },
+        properties = { tag = tags[1][1] }
+    },
+    {
+        rule = { class = "Skype" },
+        properties = { tag = tags[1][8] }
+    },
+    {
+        rule = { class = "Thunderbird" },
+        properties = { tag = tags[1][2] }
+    },
 }
 -- }}}
 
