0590a092412ea4573419be68e6c5411e4ac5d602
Martin Ueding <dev@martin-ueding.de>
2014-09-29 19:20:23 +0200

    Generate wallpaper url from hostname

--- themes/default/theme.lua
+++ themes/default/theme.lua
@@ -73,8 +73,17 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
+function get_wallpaper_name()
+    local f = io.popen("uname -snr")
+    local uname = f:read("*line")
+    f:close()
+    hostname = string.match(uname, "[%w]+[%s]([%w%p]+)[%s][%w%p]+")
+
+    return "/home/mu/.config/info-wallpaper/" .. hostname .. ".png"
+end
+
 -- You can use your own command to set your wallpaper
-theme.wallpaper_cmd = { "awsetbg /home/mu/Bilder/Hintergrundbilder/InterfaceLift/01848_flow_2560x1600.jpg" }
+theme.wallpaper_cmd = { "awsetbg " .. get_wallpaper_name() }
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
