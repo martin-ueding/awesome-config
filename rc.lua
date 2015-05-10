18117263e4160e5d855fa6c5adbc0b058faf2fb5
Martin Ueding <dev@martin-ueding.de>
2015-05-10 20:14:23 +0200

    Enable the prompt box

--- rc.lua
+++ rc.lua
@@ -365,7 +365,7 @@ for s = 1, screen.count() do
     -- Widgets that are aligned to the left
     local left_layout = wibox.layout.fixed.horizontal()
     left_layout:add(mytaglist[s])
-    --left_layout:add(mypromptbox[s])
+    left_layout:add(mypromptbox[s])
 
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
