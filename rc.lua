34152f7e89d8c2262532b0dbd68e4ecd552d86d2
Martin Ueding <dev@martin-ueding.de>
2015-06-05 11:14:40 +0200

    Use default theme, only change wallpaper

--- rc.lua
+++ rc.lua
@@ -67,8 +67,8 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
---beautiful.init("/usr/local/share/awesome/themes/default/theme.lua")
-beautiful.init("/home/mu/.config/awesome/themes/default/theme.lua")
+beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.get().wallpaper = "/home/mu/.config/wallpaper.jpg"
 
 -- This is used later as the default terminal and editor to run.
 terminal = "konsole"
