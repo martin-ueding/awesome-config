4c73d89ea3694a19814ea654d31fbad3be205c2d
Martin Ueding <dev@martin-ueding.de>
2015-10-29 18:39:42 +0100

    Remap lock button

--- rc.lua
+++ rc.lua
@@ -450,7 +450,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey }, "r", function () mypromptbox[mouse.screen]:run() end),
     awful.key({ modkey }, "d", function () awful.util.spawn("dolphin") end),
     awful.key({ modkey }, "s", function () awful.util.spawn("dolphin Dokumente/Studium/Master_Science_Physik/") end),
-    awful.key({ modkey }, "F12", function () awful.util.spawn("suspend-hook") end),
+    awful.key({ modkey }, "F2", function () awful.util.spawn("suspend-hook") end),
     awful.key({ modkey }, "F1", function () awful.util.spawn("xbacklight - 10") end),
     awful.key({ modkey }, "F2", function () awful.util.spawn("xbacklight + 10") end),
     awful.key({ modkey, "Shift" }, "x", function () awful.util.spawn("xournal") end),
